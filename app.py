223afc0849a947eb38413962306ef68c8fb99a3e
Caleb <xxgoldenprozxx@gmail.com>
2024-03-02 18:59:40 +0800

    Updated requirements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-import os
+from flask_cors import CORS
 import stripe
 
 stripe.api_key = 'sk_test_51OOBnGEvVCl2vla1w7zQ4XYBPSUslUZvifWMvfr2iji0OcoZQzfS39yYA6et6v9jKkb35D5040HdwHAvQ4fUfN7p005LTIQPJ5'
@@ -9,6 +9,7 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__, static_folder='public',
             static_url_path='', template_folder='public')
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://jesus-shirt-shop.netlify.app"]}})
 
 def calculate_order_amount(items):
     # Calculate total price for items
